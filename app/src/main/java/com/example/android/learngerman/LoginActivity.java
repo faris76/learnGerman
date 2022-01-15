a1c5b096ea7146503d856623872fb03e5f230014
Muhammad Faris Mufti <farismufti76@gmail.com>
2022-01-15 15:21:05 +0500

    authentication_backend_completed

--- app/src/main/java/com/example/android/learngerman/LoginActivity.java
+++ app/src/main/java/com/example/android/learngerman/LoginActivity.java
@@ -1,24 +1,44 @@
 package com.example.android.learngerman;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
     TextView forgotPassword,login,signUp;
     RelativeLayout emailEditText,passEditText;
     FloatingActionButton fb,insta,twitter,linkedin;
+    EditText emailEt,passEt;
     float v = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+        /**check if the user is currently logged in or not*/
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            // User is signed in
+            Intent i = new Intent(LoginActivity.this,HomeActivity.class);
+            i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(i);
+        }
 
         fb = findViewById(R.id.fb_fab);
         insta = findViewById(R.id.insta_fab);
@@ -31,13 +51,42 @@ public class LoginActivity extends AppCompatActivity {
         passEditText = findViewById(R.id.pass_et);
         signUp = findViewById(R.id.signUp);
 
+        emailEt = findViewById(R.id.username_input);
+        passEt = findViewById(R.id.pass);
 
         // Click Listeners
         signUp.setOnClickListener(v ->{
             startActivity(new Intent(this,SignupActivity.class));
         });
 
+        login.setOnClickListener(v ->{
+            loginUser();
+        });
+
         startAnimation();
+    }
+
+    private void loginUser(){
+        FirebaseAuth mAuth;
+        mAuth = FirebaseAuth.getInstance();
+
+        String email = emailEt.getText().toString();
+        String password = passEt.getText().toString();
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            finish();
+                            startActivity(new Intent(getApplicationContext(),HomeActivity.class));
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(getApplicationContext(), "failed", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 
     private void startAnimation(){
--- app/src/main/java/com/example/android/learngerman/SignupActivity.java
+++ app/src/main/java/com/example/android/learngerman/SignupActivity.java
@@ -1,31 +1,36 @@
 package com.example.android.learngerman;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SignupActivity extends AppCompatActivity {
     TextView backBtn,signUp;;
     RelativeLayout emailEditText,passEditText;
     FloatingActionButton fb,insta,twitter,linkedin;
+    EditText email_et,pass_et;
     float v = 0;
-
-    @Override
-    public void onBackPressed() {
-        super.onBackPressed();
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
-
+        // Initialization
         fb = findViewById(R.id.fb_fab);
         insta = findViewById(R.id.insta_fab);
         twitter = findViewById(R.id.twitter_fab);
@@ -37,17 +42,51 @@ public class SignupActivity extends AppCompatActivity {
         signUp = findViewById(R.id.signUp_btn);
         backBtn = findViewById(R.id.back_btn);
 
+        email_et = findViewById(R.id.email_input);
+        pass_et = findViewById(R.id.pass_input);
+
 
         backBtn.setOnClickListener(v ->{
             onBackPressed();
         });
 
+        signUp.setOnClickListener(v ->{
+            addUserWithEmail();
+        });
+
         startAnimation();
-
-
-
     }
 
+
+    private void addUserWithEmail(){
+        FirebaseAuth mAuth;
+        mAuth = FirebaseAuth.getInstance();
+
+        String email = email_et.getText().toString();
+        String password = pass_et.getText().toString();
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        // Sign in success, update UI with the signed-in user's information
+                        clear();
+                        Toast.makeText(getApplicationContext(), "User Added Successfully", Toast.LENGTH_SHORT).show();
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Toast.makeText(getApplicationContext(), "Signup Failed", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    /**To clear All Edit Text Fields
+     * in the activity after successful registration */
+    private void clear(){
+        email_et.setText("");
+        pass_et.setText("");
+    }
+
+
+    /**This methode will handle animation */
     private void startAnimation(){
         //for animation
         fb.setTranslationY(300);
@@ -85,5 +124,10 @@ public class SignupActivity extends AppCompatActivity {
         passEditText.animate().translationX(0).alpha(1).setDuration(800).setStartDelay(500).start();
         signUp.animate().translationX(0).alpha(1).setDuration(800).setStartDelay(700).start();
         backBtn.animate().translationX(0).alpha(1).setDuration(800).setStartDelay(800).start();
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
     }
 }
